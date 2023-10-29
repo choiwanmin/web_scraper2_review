6bc1655d9486f8623a62f0a6c3ff4a4d463501d6
wowmi_1 <choi.wm512@gmail.com>
2023-10-29 14:12:43 +0900

    setting.py 파일 static 관련 추가 작성

--- web_scraper2/settings.py
+++ web_scraper2/settings.py
@@ -124,6 +124,15 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS=[
+    STATIC_DIR,
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+    
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
